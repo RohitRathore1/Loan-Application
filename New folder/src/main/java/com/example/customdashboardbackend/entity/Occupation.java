cdba194fb3bdb5e0f4783897e5e1bccd4424ab81
Rohit Singh Rathaur <rohitrathore.imh55@gmail.com>
2024-05-12 13:01:05 +0530

    Bug fix

--- New folder/src/main/java/com/example/customdashboardbackend/entity/Occupation.java
+++ New folder/src/main/java/com/example/customdashboardbackend/entity/Occupation.java
@@ -1,10 +1,6 @@
 package com.example.customdashboardbackend.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 
